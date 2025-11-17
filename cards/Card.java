ecd23ecce261624d0072c0be4ffa150f25067577
mrithuns <mrithun.siva@gmail.com>
2025-11-17 23:16:11 +0000

    i dont like this

--- cards/Card.java
+++ cards/Card.java
@@ -5,6 +5,7 @@ public class Card {
     private int value;
     private static String[]SUITS = {"♥", "♠", "♣", "♦"};
     private static String[]VALUE = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    
     public Card(int su, int val) {
         suit = su;
         value = val;
--- cards/Hand.java
+++ cards/Hand.java
@@ -1,5 +1,18 @@
 package cards;
-
+import java.util.
 public class Hand {
+    private int max;
+    private Card[] cards;
+    int length=0;
+    public Hand (int max)
+    {
+        this.max=max;
+    }
+    public void add (Card c)
+    {
+        cards[length]=c;
+        length++;
+    }
+    
 
 }
