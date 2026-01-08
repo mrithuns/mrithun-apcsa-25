3608a6c73c940a8a2641c8a56ffcd6212488ebb2
mrithuns <mrithun.siva@gmail.com>
2026-01-08 22:45:28 +0000

    I completed the thing using modulus

--- warmup/Main.java
+++ warmup/Main.java
@@ -3,7 +3,14 @@ package warmup;
 public class Main {
 
     public static void main(String[] args) {
-        
-
+        int num = 0;
+        int sum = 0;
+        while (num < 1000) {
+            if (num % 3 == 0 || num % 5 == 0) {
+                sum += num;
+            }
+            num++;
+        }
+        System.out.println("The sum of all the multiples of 3 or 5 below 1000 is: " + sum);
     }
 }
